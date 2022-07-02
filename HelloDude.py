2fcc124d2a32f07104466cf5981d82a80c445c21
DavidRoFe <david.rodriguez@zoi.tech>
2022-07-02 12:28:24 +0200

    Create HelloDude.py

---
+++ HelloDude.py
@@ -0,0 +1,11 @@
+user = {'name': None,
+        'age': None,
+        'gender': None
+        }
+
+user["name"] = input('What is your name? ')
+user["age"] = input('What is your age? ')
+user["gender"] = input('What is your gender? ')
+
+print(
+    f'Hi {user["name"]}, your age is {user["age"]} y and you are {user["gender"]}')
