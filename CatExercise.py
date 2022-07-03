04027079be6f36520cc5ba64ca8fda8b3b6b943b
DavidRoFe <david.rodriguez@zoi.tech>
2022-07-03 12:00:47 +0200

    main

---
+++ CatExercise.py
@@ -0,0 +1,76 @@
+# Importing modules
+from re import T
+import urllib.request
+from random import randint
+
+# Class definition
+
+
+class Cat:
+
+    word_site = 'https://www.mit.edu/~ecprice/wordlist.10000'
+    species = 'mammal'
+
+    def __init__(self, name, age):
+
+        self._name = name
+        self._age = age
+    # End __init__()
+
+    @classmethod
+    def random_cat(cls):
+
+        response = urllib.request.urlopen(Cat.word_site)
+        long_txt = response.read().decode()
+        words = long_txt.splitlines()
+
+        return cls(words[randint(1, 9999)], randint(1, 100))
+    # End random_cat()
+
+    staticmethod
+
+    def get_oldest(cat_list):
+
+        cat_oldest = cat_list[0]
+        for item in cat_list[1::]:
+
+            if item._age > cat_oldest._age:
+
+                cat_oldest = item
+
+        return cat_oldest
+    # End get_oldest()
+
+    def create_cats(cats_no):
+
+        cat_list = []
+        cats_count = 0
+        while cats_count < cats_no:
+
+            cat_list.append(Cat.random_cat())
+
+            cats_count += 1
+
+        return cat_list
+    # End create_cats()
+
+
+# Main Logic
+while True:
+
+    cats_no = int(input(f'How many cats do you want? '))
+
+    if cats_no < 1 or cats_no > 99:
+
+        print('Enter a number between 1 and 99')
+
+    else:
+
+        break
+
+cat_list = Cat.create_cats(cats_no)
+
+cat_oldest = Cat.get_oldest(cat_list)
+
+print(
+    f'The oldest cat is {cat_oldest._age} years old and it\'s name is {cat_oldest._name}')
--- ClassManage.py
+++ ClassManage.py
@@ -4,8 +4,16 @@ from tkinter.messagebox import NO, YES
 
 
 class PlayerCharacter:
+    # Attributes
+    specie = 'Human'
+    legal_age = 18
 
     def __init__(self, name, age, gender, runing=False):
+
+        if age < PlayerCharacter.legal_age:
+
+            print(
+                f'{name}, you are too young to play this game, get older and come back!')
 
         self.name = name
         self.age = age
@@ -52,21 +60,25 @@ class PlayerCharacter:
 
 # Main program
 pc_00 = PlayerCharacter(input('What is your name: '),
-                        input('What is your age: '),
+                        int(input('What is your age: ')),
                         input('What is your gender: '))
 
-print(f'{pc_00.name} is {pc_00.age} and he is a {pc_00.gender}')
+if (pc_00.age >= PlayerCharacter.legal_age):
 
-pc_00.doChangeSpeed()
+    print(f'{pc_00.name} is {pc_00.age} and he is a {pc_00.gender}')
 
-if input(f'{pc_00.name}, do you have a mate? ') == YES:
+    pc_00.doChangeSpeed()
 
-    pc_01 = PlayerCharacter(input('What is your mate name: '),
-                            input('What is your mate age: '),
-                            input('What is your mate gender: '))
+    if input(f'{pc_00.name}, do you have a mate? ') == YES:
 
-    pc_01.doChangeSpeed()
+        pc_01 = PlayerCharacter(input('What is your mate name: '),
+                                int(input('What is your mate age: ')),
+                                input('What is your mate gender: '))
 
-else:
+        if pc_01.age >= PlayerCharacter.legal_age:
 
-    pass
+            pc_01.doChangeSpeed()
+
+    else:
+
+        pass
